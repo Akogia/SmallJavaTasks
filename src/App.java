79ac000d80ab4cfdde8e9d3b3f1ce73e60b0a2c2
Akogia <66163771+Akogia@users.noreply.github.com>
2021-09-12 22:19:25 +0200

    Almost empty git repo

---
+++ src/App.java
@@ -0,0 +1,51 @@
+public class App {
+    
+    /** 
+     * @param args
+     * @throws Exception
+     */
+
+    //static boolean contains(int wert, int[][] feld){
+    //    for(int[] i:feld){
+    //        for(int j:i){
+    //            if(wert == j){
+    //                System.out.println(j);
+    //                return true;
+//
+    //            }
+    //            if(wert < j){
+    //                System.out.println(j);
+    //                return false;
+//
+    //            }
+    //        }
+    //    }
+    //    return false;
+    //}
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //int[][] matrix = new int[][]{{1,0,2,1,4,4},{1,5,6,1,5},{15,5,8}};
+        //System.out.println(matrix[0].length);
+        //for(int i = 0; i< matrix.length; i++){
+        //    for(int j=0;j<matrix[i].length;j++){
+        //        //matrix[i][j] = i+j;
+        //        System.out.print(matrix[i][j] + " ");
+        //    }
+        //    System.out.println("");
+        //}
+        //System.out.println(matrix[1][5] + "dadsassdgdfg");
+//
+//
+//
+        //String s1 = "Hagen";
+        //String s2 = "Hagen";
+        //System.out.println(s1 == s2);
+        //String s3 = "FernUni Hagen".substring(8,13);
+        //System.out.println(s1 == s3);
+        //System.out.println(s3.equals(s2));
+        //System.out.println(s3);
+        //boolean b = App.contains(8,matrix);
+        //System.out.println(b);
+
+    }
+}
